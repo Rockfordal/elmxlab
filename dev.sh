6da05780a47a08f0c145faad991df34c66585e9d
Anders Lundström <anders@urkraft.se>
2016-06-18 14:32:46 +0200

    Nu funkar Hot reload! Använd linter för att få elmx funka i atom

--- dev.sh
+++
@@ -1,4 +0,0 @@
-echo "Dont forget to run gulp"
-cp base.html index.html
-#elm-live src/Main.elm
-elm-live src/Main.elm --output=elm.js --open
--- elm-hot-loader-starter/src/Main.elm
+++ elm-hot-loader-starter/src/Main.elm
@@ -1,11 +1,11 @@
 module Main exposing (main)
 
 import State exposing (model, update)
+import Types exposing (Model, Msg)
 import View  exposing (view)
 import Html.App as Html
 
 
-main : Program Never
 main =
   Html.beginnerProgram
     { model = model
