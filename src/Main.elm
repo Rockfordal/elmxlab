e93cb37de74d68d64943b19414572567b8afff8d
Anders Lundström <anders@urkraft.se>
2016-06-18 15:44:41 +0200

    initialmodel är bättre namn

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,6 @@
 module Main exposing (main)
 
-import State exposing (model, update)
+import State exposing (initialmodel, update)
 import View  exposing (view)
 import Html.App as Html
 
@@ -8,7 +8,7 @@ import Html.App as Html
 main : Program Never
 main =
   Html.beginnerProgram
-    { model = model
+    { model = initialmodel
     , view = view
     , update = update
     }
