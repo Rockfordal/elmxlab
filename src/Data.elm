430289bdfb14ad0ac8d36ae7754049ab2554a79b
Anders Lundström <anders@urkraft.se>
2016-06-23 15:58:08 +0200

    statisk data i egen fil (som inte behövs nu)

---
+++ src/Data.elm
@@ -0,0 +1,24 @@
+module Data exposing (..)
+import Types exposing (Post)
+
+
+p1 : Post
+p1 =
+  { title = "Texas"
+  , content = "God dag från texas här äter vi inte hundar och sover inte med ögonen öppna<b>LOOOOL</b>"
+  , id = 1
+  }
+
+p2 : Post
+p2 =
+  { title = "Stöd och matchning"
+  , content = "har går ett ex antal individer mot greatness!"
+  , id = 2
+  }
+
+p3 : Post
+p3 =
+  { title = "Grillparty"
+  , content = "vi följer vårat stolta landslag mot erövringar i europa!"
+  , id = 3
+  }
