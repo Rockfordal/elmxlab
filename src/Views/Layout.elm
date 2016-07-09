f267b22be29971c8b6523593c3ad5d94bc3654c8
Anders Lundström <anders@urkraft.se>
2016-07-09 09:18:30 +0200

    skumt var tvungen skapa en variabel

--- src/Views/Layout.elm
+++ src/Views/Layout.elm
@@ -1,6 +1,9 @@
-module Views.Shelfs exposing (..)
+module Views.Layout exposing (..)
 
 import Types exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
+
+a : Int
+a = 1
