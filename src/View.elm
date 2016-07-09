dcfd5751ab9c92d996870748b479cf830154949a
Anders Lundström <anders@urkraft.se>
2016-07-09 10:39:40 +0200

    refaktoriserat vyerna lite

--- src/View.elm
+++ src/View.elm
@@ -17,11 +17,6 @@ view model =
   in
     Html.div [] [
       navview model
-        , Html.div [Html.Attributes.attribute "class" "container"] [
-              Html.a [Html.Attributes.attribute "class" "fixed-action-btn", Html.Attributes.attribute "style" "bottom: 15px; right: 10px;"] [Html.a [Html.Attributes.attribute "class" "btn-floating btn-large teal darken-4"] [
-                Html.i [Html.Attributes.attribute "class" "large material-icons"] [Html.text "add"]
-              ]
-      ]]
-        , shelfsview model
-        , itemsview model
+      , shelfsview model
+      , itemsview model
     ]
