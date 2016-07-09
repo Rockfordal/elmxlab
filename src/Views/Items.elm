f587442822e254e3944efa5b653780e4dc7466f4
Kristoffer <landinkristoffer84@gmail.com>
2016-07-09 09:59:42 +0200

    jobbat med vy för shelfs och items anders påbörjar data för items

--- src/Views/Items.elm
+++ src/Views/Items.elm
@@ -4,16 +4,16 @@ import Types exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
-itemview : Model -> Html Msg
-itemview model =
+itemview : Item -> Html Msg
+itemview item =
   Html.div [] [
     Html.div [Html.Attributes.attribute "class" "card"] [
       Html.div [Html.Attributes.attribute "class" "input-field col col l5 m4 s4", Html.Attributes.attribute "id" "hylla 1"] [
-        Html.input [Html.Attributes.attribute "disabled" "true", Html.Attributes.attribute "value" "A01", Html.Attributes.attribute "id" "disabled", Html.Attributes.attribute "type" "text", Html.Attributes.attribute "class" "validate"] []
+        Html.input [Html.Attributes.attribute "disabled" "true", Html.Attributes.attribute "value" item.name, Html.Attributes.attribute "id" "disabled", Html.Attributes.attribute "type" "text", Html.Attributes.attribute "class" "validate"] []
         , Html.label [Html.Attributes.attribute "for" "disabled"] [Html.text "Namn"]
       ]
       , Html.div [Html.Attributes.attribute "class" "input-field col col l5 m4 s4"] [
-        Html.input [Html.Attributes.attribute "disabled" "true", Html.Attributes.attribute "value" "20", Html.Attributes.attribute "id" "disabled", Html.Attributes.attribute "type" "text", Html.Attributes.attribute "class" "validate"] []
+        Html.input [Html.Attributes.attribute "disabled" "true", Html.Attributes.attribute "value" item.info, Html.Attributes.attribute "id" "disabled", Html.Attributes.attribute "type" "text", Html.Attributes.attribute "class" "validate"] []
         , Html.label [Html.Attributes.attribute "for" "disabled"] [Html.text "Storlek"]
       ]
         , Html.div [Html.Attributes.attribute "class" "input-field col l1 m2 s2"] [
@@ -27,9 +27,12 @@ itemview model =
 
 itemsview : Model -> Html Msg
 itemsview model =
+    let
+       items = List.map itemview model.items
+    in
   Html.div [Html.Attributes.attribute "class" "container"] [
     Html.h3 [] [Html.text "Items"]
-          , Html.div [Html.Attributes.attribute "class" "row"] [
-            itemview model
-          ]
+          , Html.div [Html.Attributes.attribute "class" "row"] ([
+            ] ++ items ++ [
+          ])
       ]
--- src/Views/Shelfs.elm
+++ src/Views/Shelfs.elm
@@ -3,16 +3,16 @@ import Types exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
-shelfview : Model -> Html Msg
-shelfview model =
+shelfview : Shelf -> Html Msg
+shelfview shelf =
   Html.div [] [
     Html.div [Html.Attributes.attribute "class" "card"] [
       Html.div [Html.Attributes.attribute "class" "input-field col col l5 m4 s4", Html.Attributes.attribute "id" "hylla 1"] [
-        Html.input [Html.Attributes.attribute "disabled" "true", Html.Attributes.attribute "value" "A01", Html.Attributes.attribute "id" "disabled", Html.Attributes.attribute "type" "text", Html.Attributes.attribute "class" "validate"] []
+        Html.input [Html.Attributes.attribute "disabled" "true", Html.Attributes.attribute "value" shelf.name, Html.Attributes.attribute "id" "disabled", Html.Attributes.attribute "type" "text", Html.Attributes.attribute "class" "validate"] []
         , Html.label [Html.Attributes.attribute "for" "disabled"] [Html.text "Namn"]
       ]
       , Html.div [Html.Attributes.attribute "class" "input-field col col l5 m4 s4"] [
-        Html.input [Html.Attributes.attribute "disabled" "true", Html.Attributes.attribute "value" "20", Html.Attributes.attribute "id" "disabled", Html.Attributes.attribute "type" "text", Html.Attributes.attribute "class" "validate"] []
+        Html.input [Html.Attributes.attribute "disabled" "true", Html.Attributes.attribute "value" (toString shelf.size), Html.Attributes.attribute "id" "disabled", Html.Attributes.attribute "type" "text", Html.Attributes.attribute "class" "validate"] []
         , Html.label [Html.Attributes.attribute "for" "disabled"] [Html.text "Storlek"]
       ]
         , Html.div [Html.Attributes.attribute "class" "input-field col l1 m2 s2"] [
@@ -26,51 +26,54 @@ shelfview model =
 
 shelfsview : Model -> Html Msg
 shelfsview model =
+  let
+     shelfs = List.map shelfview model.shelfs
+  in
   Html.div [Html.Attributes.attribute "class" "container"] [
     Html.h3 [] [Html.text "Hyller"]
-          , Html.div [Html.Attributes.attribute "class" "row"] [
-            shelfview model
-          ]
+          , Html.div [Html.Attributes.attribute "class" "row"] ([
+            ] ++ shelfs ++ [
+          ])
       ]
 
-gammla : Html Msg
-gammla =
-  Html.div [Html.Attributes.attribute "class" "row"] [
-            Html.div [Html.Attributes.attribute "class" "collection"] [
-              Html.div [Html.Attributes.attribute "class" "card"] [
-                Html.div [Html.Attributes.attribute "class" "input-field col col l5 m4 s4", Html.Attributes.attribute "id" "hylla 1"] [
-                  Html.input [Html.Attributes.attribute "disabled" "true", Html.Attributes.attribute "value" "A01", Html.Attributes.attribute "id" "disabled", Html.Attributes.attribute "type" "text", Html.Attributes.attribute "class" "validate"] []
-                  , Html.label [Html.Attributes.attribute "for" "disabled"] [Html.text "Namn"]
-                ]
-                , Html.div [Html.Attributes.attribute "class" "input-field col col l5 m4 s4"] [
-                  Html.input [Html.Attributes.attribute "disabled" "true", Html.Attributes.attribute "value" "20", Html.Attributes.attribute "id" "disabled", Html.Attributes.attribute "type" "text", Html.Attributes.attribute "class" "validate"] []
-                  , Html.label [Html.Attributes.attribute "for" "disabled"] [Html.text "Storlek"]
-                ]
-              ]
-              , Html.div [Html.Attributes.attribute "class" "input-field col l1 m2 s2"] [
-                Html.i [Html.Attributes.attribute "class" "small material-icons"] [Html.text "mode_edit"]
-              ]
-              , Html.div [Html.Attributes.attribute "class" "input-field col l1 m2 s2"] [
-                Html.i [Html.Attributes.attribute "class" "small material-icons"] [Html.text "delete"]
-              ]
-          ]
-            , Html.div [Html.Attributes.attribute "class" "collection"] [
-              Html.div [Html.Attributes.attribute "class" "card"] [
-                Html.div [Html.Attributes.attribute "class" "input-field col l5 m4 s4", Html.Attributes.attribute "id" "hylla 2"] [
-                  Html.input [Html.Attributes.attribute "disabled" "true", Html.Attributes.attribute "value" "A02", Html.Attributes.attribute "id" "disabled", Html.Attributes.attribute "type" "text", Html.Attributes.attribute "class" "validate"] []
-                  , Html.label [Html.Attributes.attribute "for" "disabled"] [Html.text "Namn"]
-                ]
-                  , Html.div [Html.Attributes.attribute "class" "input-field col l5 m4 s4"] [
-                    Html.input [Html.Attributes.attribute "disabled" "true", Html.Attributes.attribute "value" "10", Html.Attributes.attribute "id" "disabled", Html.Attributes.attribute "type" "text", Html.Attributes.attribute "class" "validate"] []
-                    , Html.label [Html.Attributes.attribute "for" "disabled"] [Html.text "Storlek"]
-                  ]
-              ]
-                  , Html.div [Html.Attributes.attribute "class" "input-field col l1 m2 s2"] [
-                    Html.i [Html.Attributes.attribute "class" "small material-icons"] [Html.text "mode_edit"]
-                  ]
-                  , Html.div [Html.Attributes.attribute "class" "input-field col l1 m2 s2"] [
-                    Html.i [Html.Attributes.attribute "class" "small material-icons"] [Html.text "delete"]
-                  ]
-            ]
-        ]
-
+-- gammla : Html Msg
+-- gammla =
+--   Html.div [Html.Attributes.attribute "class" "row"] [Html.text "
+--             ", Html.div [Html.Attributes.attribute "class" "collection"] [Html.text "
+--               ", Html.div [Html.Attributes.attribute "class" "card"] [Html.text "
+--                 ", Html.div [Html.Attributes.attribute "class" "input-field col col l5 m4 s4", Html.Attributes.attribute "id" "hylla 1"] [Html.text "
+--                   ", Html.input [Html.Attributes.attribute "disabled" "true", Html.Attributes.attribute "value" "A01", Html.Attributes.attribute "id" "disabled", Html.Attributes.attribute "type" "text", Html.Attributes.attribute "class" "validate"] [], Html.text "
+--                   ", Html.label [Html.Attributes.attribute "for" "disabled"] [Html.text "Namn"], Html.text "
+--                 "], Html.text "
+--                 ", Html.div [Html.Attributes.attribute "class" "input-field col col l5 m4 s4"] [Html.text "
+--                   ", Html.input [Html.Attributes.attribute "disabled" "true", Html.Attributes.attribute "value" "20", Html.Attributes.attribute "id" "disabled", Html.Attributes.attribute "type" "text", Html.Attributes.attribute "class" "validate"] [], Html.text "
+--                   ", Html.label [Html.Attributes.attribute "for" "disabled"] [Html.text "Storlek"], Html.text "
+--                 "], Html.text "
+--               "], Html.text "
+--               ", Html.div [Html.Attributes.attribute "class" "input-field col l1 m2 s2"] [Html.text "
+--                 ", Html.i [Html.Attributes.attribute "class" "small material-icons"] [Html.text "mode_edit"], Html.text "
+--               "], Html.text "
+--               ", Html.div [Html.Attributes.attribute "class" "input-field col l1 m2 s2"] [Html.text "
+--                 ", Html.i [Html.Attributes.attribute "class" "small material-icons"] [Html.text "delete"], Html.text "
+--               "], Html.text "
+--           "], Html.text "
+--             ", Html.div [Html.Attributes.attribute "class" "collection"] [Html.text "
+--               ", Html.div [Html.Attributes.attribute "class" "card"] [Html.text "
+--                 ", Html.div [Html.Attributes.attribute "class" "input-field col l5 m4 s4", Html.Attributes.attribute "id" "hylla 2"] [Html.text "
+--                   ", Html.input [Html.Attributes.attribute "disabled" "true", Html.Attributes.attribute "value" "A02", Html.Attributes.attribute "id" "disabled", Html.Attributes.attribute "type" "text", Html.Attributes.attribute "class" "validate"] [], Html.text "
+--                   ", Html.label [Html.Attributes.attribute "for" "disabled"] [Html.text "Namn"], Html.text "
+--                 "], Html.text "
+--                   ", Html.div [Html.Attributes.attribute "class" "input-field col l5 m4 s4"] [Html.text "
+--                     ", Html.input [Html.Attributes.attribute "disabled" "true", Html.Attributes.attribute "value" "10", Html.Attributes.attribute "id" "disabled", Html.Attributes.attribute "type" "text", Html.Attributes.attribute "class" "validate"] [], Html.text "
+--                     ", Html.label [Html.Attributes.attribute "for" "disabled"] [Html.text "Storlek"], Html.text "
+--                   "], Html.text "
+--               "], Html.text "
+--                   ", Html.div [Html.Attributes.attribute "class" "input-field col l1 m2 s2"] [Html.text "
+--                     ", Html.i [Html.Attributes.attribute "class" "small material-icons"] [Html.text "mode_edit"], Html.text "
+--                   "], Html.text "
+--                   ", Html.div [Html.Attributes.attribute "class" "input-field col l1 m2 s2"] [Html.text "
+--                     ", Html.i [Html.Attributes.attribute "class" "small material-icons"] [Html.text "delete"], Html.text "
+--                   "], Html.text "
+--             "], Html.text "
+--         "]
+-- 
